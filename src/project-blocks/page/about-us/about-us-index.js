1d78e64267c48921fe1796189f57d12decca723b
pest2811@gmail.com <pest2811@gmail.com>
2022-08-23 22:11:06 +0300

    Update logo animation in header & community block

--- src/project-blocks/page/about-us/about-us-index.js
+++ src/project-blocks/page/about-us/about-us-index.js
@@ -1,1 +1,11 @@
 import "../../../index.scss";
+
+const circles = document.querySelectorAll("#circle");
+const atom = document.querySelector("#atom");
+
+circles.forEach((circle) => {
+  atom.addEventListener("mouseover", () => {
+    circle.setAttribute("cx", "0");
+    circle.setAttribute("cy", "0");
+  });
+});
--- src/project-blocks/page/members/members-index.js
+++ src/project-blocks/page/members/members-index.js
@@ -1,2 +1,11 @@
 import "../../../index.scss";
-console.log("hi");
+
+const circles = document.querySelectorAll("#circle");
+const atom = document.querySelector("#atom");
+
+circles.forEach((circle) => {
+  atom.addEventListener("mouseover", () => {
+    circle.setAttribute("cx", "0");
+    circle.setAttribute("cy", "0");
+  });
+});
