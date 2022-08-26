e01bc9fc65ef7ee752d1fded55177766660182b9
pest2811@gmail.com <pest2811@gmail.com>
2022-08-26 23:21:00 +0300

    Fix transition of buttons

--- src/project-blocks/page/members/members-index.js
+++ src/project-blocks/page/members/members-index.js
@@ -34,9 +34,60 @@ modal.addEventListener("click", (e) => {
 });
 
 
-const btns = document.querySelectorAll('.btn');
-btns.forEach(btn => {
-  btn.addEventListener("click", function () {
-    this.classList.toggle('is-checked');
+const btns = document.querySelectorAll(".btn"),
+      list = document.querySelectorAll(".members__item");
+
+btns.forEach( (btn) => {
+  btn.addEventListener('click', ()=> {
+     btn.classList.toggle("is-checked");
   });
 });
+
+// document.querySelector('.members__buttons').addEventListener('click', (evt)=> {
+//     if(evt.target.tagName !== 'BUTTON') return false;
+//     let filterClass = evt.target.dataset['filter'];
+//     evt.target.classList.toggle("is-checked");
+//     list.forEach((el) => {
+//       el.classList.remove("hide");
+//       if (!el.classList.contains(filterClass)) {
+//         el.classList.add("hide");
+//       }
+//     });
+
+
+//   });
+
+
+// btns.forEach(btn => {
+//   btn.addEventListener("click", function (evt) {
+//     let filterClass = evt.target.dataset["filter"];
+//     evt.target.classList.toggle("is-checked");
+//     list.forEach((el) => {
+//       el.classList.remove("hide");
+//       if (!el.classList.contains(filterClass)) {
+//         el.classList.add("hide");
+//       }
+//     });
+//   }); 
+// });
+
+// function filter(category, items, btn){
+//     items.forEach( (item) => {
+//       const isItemFiltered = !item.classList.contains(category);
+//       if(isItemFiltered ){
+//         item.classList.add('hide');
+//         btn.classList.add("is-checked");
+//       } else {
+//         item.classList.remove('hide');
+//         btn.classList.remove("is-checked");
+//       }
+//     });
+// }
+// btns.forEach( (btn)=> {
+//   btn.addEventListener('click', (evt) => {
+//     const target = evt.target;
+//     const currentCategory = btn.dataset.filter;
+//     filter(currentCategory,list, target);
+//   });
+// });
+
