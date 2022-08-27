9d8b44fd3124efc056539b4baf73ba18c8e5be1f
pest2811@gmail.com <pest2811@gmail.com>
2022-08-28 00:42:54 +0300

    Add mixin for underline animation, add input-range element(need to optimize for cross-browser using)

--- src/project-blocks/page/members/members-index.js
+++ src/project-blocks/page/members/members-index.js
@@ -43,6 +43,24 @@ btns.forEach( (btn) => {
   });
 });
 
+const range = document.querySelector('.range-input'),
+      output = document.querySelector('.range-output');
+
+range.addEventListener('input', ()=> {
+  setOutput(range,output);
+});
+setOutput(range, output);
+
+function setOutput(range,output) {
+  const val = range.value;
+  const min = range.min ? range.min : 0;
+  const max = range.max ? range.max : 100;
+  const newValue = Number(((val - min) * 100) / (max - min));
+
+  output.innerHTML = val;
+  output.style.left = `calc(${newValue}% + (${14 - newValue * 0.25}px))`;
+}
+
 // document.querySelector('.members__buttons').addEventListener('click', (evt)=> {
 //     if(evt.target.tagName !== 'BUTTON') return false;
 //     let filterClass = evt.target.dataset['filter'];
