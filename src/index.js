b0e55c619cfcab75df15639122d06a1234e3dff8
pest2811@gmail.com <pest2811@gmail.com>
2022-08-30 21:52:20 +0300

    Finish members page, projects page

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import "./index.scss";
 
-const circles = document.querySelectorAll("#circle");
-const atom = document.querySelector("#atom");
+window.addEventListener('DOMContentLoaded', ()=> {
+  //atom animation
+  const circles = document.querySelectorAll("#circle");
+  const atom = document.querySelector("#atom");
 
-circles.forEach((circle) => {
-  atom.addEventListener("mouseover", () => {
-    circle.setAttribute("cx", "0");
-    circle.setAttribute("cy", "0");
+  circles.forEach((circle) => {
+    atom.addEventListener("mouseover", () => {
+      circle.setAttribute("cx", "0");
+      circle.setAttribute("cy", "0");
+    });
   });
+  
 });
